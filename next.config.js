9d301219fc246e6fbf6e06983ea1365a29f2368c
Josh Daniel <joshxfi.dev@gmail.com>
2022-06-22 20:18:49 +0800

    chore: update deps

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config) => {
+    if (!config.experiments) {
+      config.experiments = {};
+    }
+    config.experiments.topLevelAwait = true;
+    return config;
+  },
+  images: {
+    domains: ["robohash.org"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
